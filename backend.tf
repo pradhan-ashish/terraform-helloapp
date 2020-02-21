2065e0d5e2b2d3dadc17b144b600d4440cbcabc5
ashish <ashish_s98@hotmail.com>
2020-02-20 20:09:54 -0500

    added path and credentials to both backend and provider

--- backend.tf
+++ backend.tf
@@ -3,6 +3,7 @@ terraform {
     bucket = "terraform-ashishpradhan"
     key    = "terraform/dev/helloapp"
     region = "us-east-1"
-    
+    shared_credentials_files = "/home/intelycorellc/.aws/credentials"
+    profile = "ashishpradhan"
   }
 }
--- provider.tf
+++ provider.tf
@@ -1,4 +1,5 @@
 provider "aws" {
 	region = "us-east-1"
-	
+	shared_credentials_files = "/home/intelycorellc/.aws/credentials"
+	profile = "ashishpradhan"
 }
